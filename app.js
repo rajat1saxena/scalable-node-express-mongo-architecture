20ecfd6ba151418ab5ee66965ce7ccd1c4963da3
Rajat Saxena <rajat@raynstudios.in>
2017-03-08 15:13:29 +0530

    Corrected routes structure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var db = require('./mongoose');
-var models = require('./models')
+var models = require('./models');
 
 var express = require('express');
 var path = require('path');
@@ -8,8 +8,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var router = require('./routes');
 
 var app = express();
 
@@ -25,8 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
